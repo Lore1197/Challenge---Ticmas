53558feb780e31402d7d1d12d6caa741317ecac0
Lore1197 <138728114+Lore1197@users.noreply.github.com>
2024-09-30 11:51:13 -0300

    Update index.js

--- index.js
+++ index.js
@@ -6,24 +6,19 @@ const {router2} = require('./routes/pendiente')
 const {router3} = require('./routes/estado')
 const {router4} = require('./routes/diastranscurridos')
 
-// 1. Middleware setup
 server.use(express.json());
 
-// 2. Register routes
 server.use('/api/tareas', router);
 server.use('/api/tarea', router2);
 server.use('/api/estado', router3);
 server.use('/api/diastranscurridos', router4);
 
-// 3. Route handler
 server.get('/', (req, res) => {
   res.send('Server is on');
 });
 
-// 4. Server connection initialization (e.g., database connection)
 serverConnection();
 
-// 5. Start server and listen on port
 const port = 8080;
 server.listen(port, () => {
   console.log(`The server is running on port: ${port}`);
